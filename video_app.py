4f860868a926a1709c9a51b791dccbfe0a5faac9
Syudai1010 <gilbertallout@docomo.ne.jp>
2024-11-22 15:10:40 +0900

    Update video_app.py

--- video_app.py
+++ video_app.py
@@ -1,23 +1,23 @@
 import streamlit as st
 
-# Google Driveの動画リンクを再生可能な形式に変換
+# Vimeoの埋め込みリンク
 video_links = {
-    "Video 1": "https://drive.google.com/uc?id=17r_7nOhtB5M2u3QI86vEWtPWgQG-e18C",
-    "Video 2": "https://drive.google.com/uc?id=1ESqo0wQF8jEq5sqZWbQ7OyreQh8E4bWF",
-    "Video 3": "https://drive.google.com/uc?id=1OVfIqaGQcfEupYCPJ1VF0roL9arx9jr6",
+    "Vimeo Video 1": "https://player.vimeo.com/video/1032216683",
+    "Vimeo Video 2": "https://player.vimeo.com/video/1032216715",
+    "Vimeo Video 3": "https://player.vimeo.com/video/1032216750",
 }
 
 # アプリのタイトル
-st.title("Google Drive 動画再生アプリ")
+st.title("Vimeo 動画再生アプリ")
 
 # 動画選択のドロップダウンメニュー
 selected_video = st.selectbox("再生する動画を選択してください", list(video_links.keys()))
 
-# 選択された動画のリンクを取得
+# 選択された動画の埋め込みリンクを取得
 video_url = video_links[selected_video]
 
 # 動画を再生
 st.video(video_url)
 
 # 簡単な説明
-st.write("Google Driveに保存された動画を選択して再生できます。")
+st.write("Vimeoに保存された動画を選択して再生できます。")
